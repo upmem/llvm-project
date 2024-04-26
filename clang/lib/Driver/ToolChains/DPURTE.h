3a5a75c6771bb25144985c0824a258988196a833
Willy Wolff <wwolff@upmem.com>
2024-04-26 18:03:34 +0200

    dpu: llvm: DPURTE: cleanup cc1 linker

--- clang/lib/Driver/ToolChains/DPURTE.h
+++ clang/lib/Driver/ToolChains/DPURTE.h
@@ -25,36 +25,7 @@ class LLVM_LIBRARY_VISIBILITY DPURTE : public Generic_ELF {
 public:
   DPURTE(const Driver &D, const llvm::Triple &Triple,
          const llvm::opt::ArgList &Args)
-      : Generic_ELF(D, Triple, Args) {
-    PathToStdlibIncludes = GetUpmemSdkPath("/share/upmem/include/stdlib");
-    PathToSyslibIncludes = GetUpmemSdkPath("/share/upmem/include/syslib");
-    PathToLinkScript = GetUpmemSdkPath("/share/upmem/include/link/dpu.lds");
-    PathToRtLibDirectory = GetUpmemSdkPath("/share/upmem/include/built-in");
-    RtLibName = Args.hasArg(options::OPT_pg) ? "rt_p" : "rt";
-    if (Args.hasArg(options::OPT_pg)) {
-      PathToRtLibLTO =
-          GetUpmemSdkPath("/share/upmem/include/built-in/librtlto_p.a");
-      PathToRtLibLTOThin =
-          GetUpmemSdkPath("/share/upmem/include/built-in/librtltothin_p.a");
-      PathToBootstrap = GetUpmemSdkPath("/share/upmem/include/misc/crt0_p.o");
-    } else {
-      PathToRtLibLTO =
-          GetUpmemSdkPath("/share/upmem/include/built-in/librtlto.a");
-      PathToRtLibLTOThin =
-          GetUpmemSdkPath("/share/upmem/include/built-in/librtltothin.a");
-      PathToBootstrap = GetUpmemSdkPath("/share/upmem/include/misc/crt0.o");
-    }
-    McountLibName = "rtmcount";
-  }
-
-  ~DPURTE() override {
-    free(PathToStdlibIncludes);
-    free(PathToSyslibIncludes);
-    free(PathToLinkScript);
-    free(PathToRtLibDirectory);
-    free(PathToRtLibLTO);
-    free(PathToRtLibLTOThin);
-  }
+      : Generic_ELF(D, Triple, Args) {}
 
   unsigned GetDefaultDwarfVersion() const override { return 4; }
 
@@ -77,41 +48,16 @@ public:
                         llvm::opt::ArgStringList &CC1Args,
                         Action::OffloadKind DeviceOffloadKind) const override;
 
+  std::string computeSysRoot() const override;
 protected:
   Tool *buildLinker() const override;
-
-private:
-  char *GetUpmemSdkPath(const char *Path);
-
-  char *PathToSDK = NULL;
-  char *PathToSyslibIncludes;
-  char *PathToStdlibIncludes;
-  char *PathToLinkScript;
-  char *PathToRtLibDirectory;
-  const char *RtLibName;
-  char *PathToRtLibLTO;
-  char *PathToRtLibLTOThin;
-  char *PathToBootstrap;
-  const char *McountLibName;
 };
 } // end namespace toolchains
 namespace tools {
 namespace dpu {
 class LLVM_LIBRARY_VISIBILITY Linker : public Tool {
 public:
-  Linker(const ToolChain &TC, const char *Script, const char *RtLibDir,
-         const char *RtLibName, const char *PathToRtLibLTO,
-         const char *PathToRtLibLTOThin, const char *PathToBootstrap,
-         const char *McountLibName)
-      : Tool("dpu::Linker", "ld.lld", TC) {
-    LinkScript = Script;
-    RtLibraryPath = RtLibDir;
-    RtLibraryName = RtLibName;
-    RtLTOLibrary = PathToRtLibLTO;
-    RtLTOThinLibrary = PathToRtLibLTOThin;
-    Bootstrap = PathToBootstrap;
-    McountLibraryName = McountLibName;
-  }
+  Linker(const ToolChain &TC) : Tool("dpu::Linker", "ld.lld", TC) {}
 
   bool isLinkJob() const override { return true; }
 
@@ -121,15 +67,6 @@ public:
                     const InputInfo &Output, const InputInfoList &Inputs,
                     const llvm::opt::ArgList &TCArgs,
                     const char *LinkingOutput) const override;
-
-private:
-  const char *LinkScript;
-  const char *RtLibraryPath;
-  const char *RtLibraryName;
-  const char *RtLTOLibrary;
-  const char *RtLTOThinLibrary;
-  const char *Bootstrap;
-  const char *McountLibraryName;
 };
 } // end namespace dpu
 } // end namespace tools
